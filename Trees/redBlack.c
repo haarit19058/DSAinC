eea0acc9896f3db13d083dab16e4c6ea103ca969
Haarit <haarit1905@gmail.com>
2024-12-31 14:58:08 +0530

    regular update

---
+++ Trees/redBlack.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+Properties of red black trees
+1) Each node is either red or black
+2) Root is always black
+3) Red nodes cannot have red children
+4) Every path from a black node ot leaves has same number of black nodes
+5) All leaves are black
+
+Derive that the max height of the tree is <=2log(base2)(n+1)
+
+
+*/
+
+int main(){
+	printf("Hello World\n");
+	return 0;
+}
